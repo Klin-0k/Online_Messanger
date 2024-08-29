2d60e79e24148646eb99e22a04ca3ea2f593bce0
Ivan Klinitski <klinok@Ivans-MacBook-Air.local>
2024-08-30 01:04:03 +0300

    ch_deb_mod

--- django_site/settings.py
+++ django_site/settings.py
@@ -32,7 +32,7 @@ else:
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = is_password_in_file
+DEBUG = True
 
 if is_password_in_file:
     ALLOWED_HOSTS = password.ALLOWED_HOSTS
